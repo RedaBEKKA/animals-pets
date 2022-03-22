4323a6c13c4cb5a0fe4c45acc86e69eefab9f7fd
amariche <insigned12@gmail.com>
2022-03-22 18:39:38 +0100

    fix bugrs

--- src/pages/Register/Components/Form1/Components/Button.js
+++ src/pages/Register/Components/Form1/Components/Button.js
@@ -1,40 +1,36 @@
-import { Box, Button } from '@material-ui/core';
-import React from 'react'
-import { Link } from 'react-router-dom';
+import { Box, Button } from "@material-ui/core";
+import React from "react";
+import { Link } from "react-router-dom";
 import Alert from "@material-ui/lab/Alert";
-import { colors } from '../../../../../themes/colors';
-import { useStyles } from '../Styles';
+import { colors } from "../../../../../themes/colors";
+import { useStyles } from "../Styles";
 
-
-function ButtonContainer({formik,ActivateAlert,onSubmit,NextPage}) {
+function ButtonContainer({ formik, ActivateAlert, onSubmit, NextPage }) {
   const classes = useStyles();
-
-
-
 
   return (
     <Box className={classes.containerButtons}>
-    <Link to="/signIn" style={{ paddingLeft: 15 }}>
-      {" "}
-      Déja un compte ? Se connecter
-    </Link>
-    <Button
-      onClick={() => {
-        NextPage();
-      }}
-      variant="contained"
-      style={{
-        backgroundColor: colors.brown,
-        color: colors.white,
-        fontWeight: "700",
-      }}
-      className={classes.btn}
-      disabled={formik?.isSubmitting ? true : false}
-    >
-      Suivant &gt;
-    </Button>
-  </Box>
-  )
+      <Link to="/signIn" style={{ paddingLeft: 15 }}>
+        {" "}
+        Déja un compte ? Se connecter
+      </Link>
+      <Button
+        onClick={() => {
+          NextPage();
+        }}
+        variant="contained"
+        style={{
+          backgroundColor: colors.brown,
+          color: colors.white,
+          fontWeight: "700",
+        }}
+        className={classes.btn}
+        disabled={formik?.isSubmitting ? true : false}
+      >
+        Suivant &gt;
+      </Button>
+    </Box>
+  );
 }
 
-export default ButtonContainer
+export default ButtonContainer;
--- src/pages/Register/Components/Form2/From2.js
+++ src/pages/Register/Components/Form2/From2.js
@@ -24,15 +24,22 @@ function From2({ onReturn, TypeLenseigne, typeInscrire, formik, TypeOffre }) {
 
 export default From2;
 
-{
-  /* {TypeLenseigne.map((option) => {
-          return (
-            <>
-              <Field type='checkbox' name="TypeLenseigne" value={option.value} />
-            </>
-          );
-        })} */
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 {
   /* <Box className={classes.inputBox}>
 <Select
@@ -44,14 +51,7 @@ export default From2;
 />
 </Box> */
 }
-// const MenuProps = {
-//   PaperProps: {
-//     style: {
-//       maxHeight: 48 * 4.5 + 8,
-//       width: 400,
-//     },
-//   },
-// };
+
 // <PlacesAutocomplete
 // //   value={adresse}
 // //   onChange={setAdresse}
@@ -89,109 +89,5 @@ export default From2;
 //     </div>
 //   )}
 // </PlacesAutocomplete>
-{
-  /* <FormControl
-component="fieldset"
-className={classes.formControl}
-required={true}
->
-<FormLabel
-  component="legend"
-  style={{ fontWeight: "800", color: "#000" }}
->
-  Type de l'enseigne
-</FormLabel>
-<FormGroup>
-  <FormControlLabel
-    control={
-      <Checkbox
-        //   checked={estArtisan}
-        //   onChange={(e) => setEstArtisan(e.target.checked)}
-        name="estArtisan"
-        color="default"
-      />
-    }
-    label="Artisan"
-  />
-  <FormControlLabel
-    control={
-      <Checkbox
-        //   checked={estMagasinSpe}
-        //   onChange={(e) => setEstMagasinSpe(e.target.checked)}
-        name="estMagasinSpe"
-        color="default"
-      />
-    }
-    label="Magasin spécialisé"
-  />
-  <FormControlLabel
-    control={
-      <Checkbox
-        //   checked={estMagasinGen}
-        //   onChange={(e) => setEstMagasinGen(e.target.checked)}
-        name="estMagasinGen"
-        color="default"
-      />
-    }
-    label="Magasin généraliste"
-  />
-  <FormControlLabel
-    control={
-      <Checkbox
-        //   checked={estAsso}
-        //   onChange={(e) => setEstAsso(e.target.checked)}
-        name="estAsso"
-        color="default"
-      />
-    }
-    label="Association"
-  />
-  <FormControlLabel
-    control={
-      <Checkbox
-        //   checked={estMarque}
-        //   onChange={(e) => setEstMarque(e.target.checked)}
-        name="estMarque"
-        color="default"
-      />
-    }
-    label="Marque"
-  />
 
-  <FormControlLabel
-    control={
-      <Checkbox
-        //   checked={estMarque}
-        //   onChange={(e) => setEstMarque(e.target.checked)}
-        name="MarketPlace"
-        color="default"
-      />
-    }
-    label="MarketPlace"
-  />
-</FormGroup>
-</FormControl> */
-}
 
-{
-  /* <Select
-                  multiple
-                  value={nomsServicesCHoisis}
-                  onChange={handleChange}
-                  input={<Input id="select-multiple-checkbox" />}
-                  renderValue={(selected) => selected.join(", ")}
-                  MenuProps={MenuProps}
-                > */
-}
-{
-  /* {nomsServices.map((name) => (
-                  <MenuItem key={name} value={name}>
-                    <Checkbox
-                      checked={nomsServicesCHoisis.indexOf(name) > -1}
-                    />
-                    <ListItemText primary={name} />
-                  </MenuItem>
-                ))} */
-}
-// </Select>
-//
